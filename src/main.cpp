6737c7c8b1f082aac79949ef2c490bc9c6c56747
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-02-02 11:06:16 -0500

    prototype removed for branching

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,6 @@
 const int RST_PIN = 22; // Reset pin
 const int SS_PIN = 21;  // Slave select pin
 
-bool adding = 0;
-bool authorisation = 0;
-
-String masterCard = "72 0C AA 1B";
-String cards[] = {"B2 A8 3F 61"};
-
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
 /**
@@ -40,54 +34,17 @@ void loop()
 
     if (!mfrc522.PICC_ReadCardSerial()) { return; }
 
-    Serial.print("UID tag : ");
-    String content = "";
-    byte letter;
+    String card = "";
 
     for (byte i = 0; i < mfrc522.uid.size; i++)
     {
-        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-        content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-        content.concat(String(mfrc522.uid.uidByte[i], HEX));
+        card.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+        card.concat(String(mfrc522.uid.uidByte[i], HEX));
     }
 
-    Serial.println();
-    content.toUpperCase();
+    card.toUpperCase();
+    Serial.print("UID tag : ");
+    Serial.println(card);
 
-    if (adding)
-    {
-        Serial.println("Next card will be added .... ");
-        //cards[(sizeof(cards) / sizeof(int)) + 1] = content.substring(1); // marche pas ca
-    }
-    else
-    {
-        for (byte i = 0; i < sizeof(cards) / sizeof(int); i++)
-        {
-
-            if (content.substring(1) == masterCard)
-            {
-                //adding = 1;
-                break;
-            }
-            else
-            {
-                if (content.substring(1) == cards[i])
-                {
-                    authorisation = 1;
-                }
-            }
-        }
-
-        if (authorisation)
-        {
-            Serial.println("access oui");
-        }
-        else
-        {
-            Serial.println("non tu non non");
-        }
-        authorisation = 0;
-    }
     delay(1000);
 }
