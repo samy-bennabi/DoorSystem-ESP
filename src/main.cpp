e494288588354984643758eed4c46645b42e64ff
Devloon <99757181+devloon@users.noreply.github.com>
2023-04-06 12:27:39 -0400

    Code cleanup and OO removed all code for wifi from main, all managed in myWiFi class. added myMqtt and myRfid classes to cleanup more removed mqtt code from main (untested, definetly not working) rfid still in main for now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,34 @@
-#include <WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <PubSubClient.h>
 #include "myWiFi.h"
-
-// Wifi credentianls
-const char *ssid = "EcoleDuWeb2.4g";
-const char *password = "EcoleDuWEB";
+#include "myMqtt.h"
 
 // pins pour la communication SPI avec le lecteur RFID-RC522
+#define RELAY_PIN 15 // pin for relay
 #define ONBOARD_LED_PIN 2
 #define RST_PIN 22 // Reset pin
 #define SS_PIN 21  // Slave select pin
-#define RELAY_PIN 15
 
-// variables et constantes pour la communication avec le mqtt broker
-char *mqttServer = "172.16.5.101";
-int mqttPort = 1883;
-const char *mqtt_client_name = "ESP32";
-const char *mqtt_pub_check = "/door/card/check"; // The topic to which our client will publish
-const char *mqtt_pub_add = "/door/card/add"; // The topic to which our client will publish
-const char *mqtt_sub_topic = "/door/acsLvl";  // The topic to which our client will subscribe
+// default Wifi credentianls
+const char *ssid = "EcoleDuWeb2.4g";
+const char *password = "EcoleDuWEB";
+
+// const and var for mqtt connection and communication
+const char *mqttServer        = "172.16.5.101";
+const int   mqttPort          = 1883;
+const char *mqtt_client_name  = "ESP32";
+const char *mqtt_pub_check    = "/door/card/check"; // The topic to which our client will publish
+const char *mqtt_pub_add      = "/door/card/add"; // The topic to which our client will publish
+const char *mqtt_sub_topic    = "/door/acsLvl";  // The topic to which our client will subscribe
 
 //__________________________________________________________________________________________________________
 
-WiFiClient client;
-PubSubClient mqttClient(client);
-
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
+// my classes
+myWiFi wifi;
+MyMqtt mqtt(mqttServer, mqttPort,  mqtt_client_name, "", "", mqtt_sub_topic);
 // variables pour la gestion des etats
 String card="";
 byte acsLvl=0;
@@ -39,72 +39,31 @@ bool auth=0;
 String masterCard = "72 0C AA 1B";
 String cards[] = {"B2 A8 3F 61", "DC 33 75 32"};
 
-// callback to be executed when the subscribed-to topic has a pub
-void callback(char *topic, byte *payload, unsigned int length){
-  Serial.print("Message received from ");
-  Serial.print(topic);
-  Serial.print(" ");
-  for (int i = 0; i < length; i++) { acsLvl = (payload[i] - '0'); }
-  Serial.print(acsLvl);
-
-  // gestion des etats selon la reponse de l'api
-  switch (acsLvl) {
-  case 1: auth=1; break;
-  case 2: add=20; break;
-    default: break;
-  }
-
-  Serial.println();
-}
-
 /**
  * Initialize.
  */
 void setup(){
   Serial.begin(115200); // Initialize serial communications with the PC
-  WiFi.mode(WIFI_STA);  // The WiFi is in station mode
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  // afficher les infos de la connexion dans le terminal
-  /*Serial.println("");
-  Serial.print("WiFi connected to: ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Hol'up a minute");*/
-
-  delay(3000);
-
-  mqttClient.setServer(mqttServer, mqttPort); // connexion au broker mqtt
-  mqttClient.setCallback(callback); // initialisation du callback
-
-  // configuration de la pin pour le relai
+  
+  // pin setup for relay
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW); // s'assurer que c'est ferme par defaut
-
-  while (!Serial)
-    ; // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-
+  digitalWrite(RELAY_PIN, LOW); // make sure the relay is off
+  
   SPI.begin(); // Init SPI bus
   mfrc522.PCD_Init();
   delay(10);
-  //mfrc522.PCD_DumpVersionToSerial(); // remove in prod
-  if (!mqttClient.connected())
-  {
-    while (!mqttClient.connected())
-    {
-      if (mqttClient.connect(mqtt_client_name))
-      {
-        Serial.println("MQTT Connected!");
-        mqttClient.subscribe(mqtt_sub_topic);
-      }
-      else { Serial.print(".");}
-    }
+
+  // wifi connection
+  if( !wifi.connectToWiFi(ssid, password)){
+    wifi.startAPMode("ESP32", "Patate123");
   }
+  delay(3000);
+
+  // mqtt connection
+  mqtt.setup();
+
+  while (!Serial)
+    ; // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 
   Serial.println("Lessgo scan...");
 }
@@ -114,17 +73,7 @@ void setup(){
  */
 void loop(){
   //check mqtt and reconnect if disconnected
-  if (!mqttClient.connected()) {
-    while (!mqttClient.connected())
-    {
-      if (mqttClient.connect(mqtt_client_name))
-      {
-        Serial.println("MQTT Connected!");
-        mqttClient.subscribe(mqtt_sub_topic);
-      }
-      else { Serial.print("."); }
-    }
-  }
+  mqtt.refresh();
 
   if (mfrc522.PICC_IsNewCardPresent()) // when card is scanned
   {
@@ -145,7 +94,7 @@ void loop(){
     }
   }
 
-  mqttClient.loop(); // loop pour continuer a verifier les messages
+  mqtt.loop(); // loop pour continuer a verifier les messages
   
   // pour ouvrir la porte si la carte est authentifiee
   if (auth) {
@@ -164,4 +113,22 @@ void loop(){
   }
 
   delay(250);
+}
+
+// callback to be executed when the subscribed-to topic has a pub
+void callback(char *topic, byte *payload, unsigned int length){
+  Serial.print("Message received from ");
+  Serial.print(topic);
+  Serial.print(" ");
+  for (int i = 0; i < length; i++) { acsLvl = (payload[i] - '0'); }
+  Serial.print(acsLvl);
+
+  // gestion des etats selon la reponse de l'api
+  switch (acsLvl) {
+  case 1: auth=1; break;
+  case 2: add=20; break;
+    default: break;
+  }
+
+  Serial.println();
 }
