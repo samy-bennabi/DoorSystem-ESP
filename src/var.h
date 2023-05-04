4b6ee43215848c7a0b078d8354cc6ffef408a16f
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-05-04 11:02:09 -0400

    bug fixes

--- src/var.h
+++ src/var.h
@@ -12,8 +12,8 @@
 #define ONBOARD_LED_PIN 2
 
 // default Wifi credentianls
-const char *ssid = "P500";
-const char *password = "Patate123";
+const char *ssid = "X280";
+const char *password = "Chepo123";
 
 // const and var for mqtt connection and communication
 const char *doorName        = "C-089";
@@ -28,9 +28,9 @@ const int   mqttPort        = 1883;
 const char *mqtt_user       = "user";
 const char *mqtt_password   = "Patate123";
 const char *mqtt_id         = "ESP32";
-const char *mqtt_pub_check  = "/DoorSystem/card/check"; // The topic to which our client will publish
-const char *mqtt_pub_add    = "/DoorSystem/card/add"; // The topic to which our client will publish
-const char *mqtt_sub_access = "/DoorSystem/door/open";  // The topic to which our client will subscribe
-const char *mqtt_sub_add    = "/DoorSystem/card/add";  // The topic to which our client will subscribe
+const char *mqtt_pub_check  = "DoorSystem/card/check"; // The topic to which our client will publish
+const char *mqtt_pub_add    = "DoorSystem/card/add"; // The topic to which our client will publish
+const char *mqtt_sub_access = "DoorSystem/door/open";  // The topic to which our client will subscribe
+const char *mqtt_sub_add    = "DoorSystem/card/add";  // The topic to which our client will subscribe
 
 #endif
