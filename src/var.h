1069c956ade168aa09418ea544db9f158b26212b
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-05-04 18:51:19 -0400

    access adding mqtt topics inconsistency resolved variable names changed

--- src/var.h
+++ src/var.h
@@ -17,20 +17,23 @@ const char *password = "Chepo123";
 
 // const and var for mqtt connection and communication
 const char *doorName        = "C-089";
+const int   door_open_delay = 2000; // in milliseconds
 
 const char *apiServer       = "137.184.168.254";
 const int   apiPort         = 8080;
-const char *api_card_check  = "/api/access/check";
 const char *api_card_add    = "/api/card/add";
+const char *api_access_add  = "/api/access/add";
+const char *api_access_check= "/api/access/check";
 
 const char *mqttServer      = "137.184.168.254";
 const int   mqttPort        = 1883;
 const char *mqtt_user       = "user";
 const char *mqtt_password   = "Patate123";
 const char *mqtt_id         = "ESP32";
-const char *mqtt_pub_check  = "DoorSystem/card/check"; // The topic to which our client will publish
-const char *mqtt_pub_add    = "DoorSystem/card/add"; // The topic to which our client will publish
-const char *mqtt_sub_access = "DoorSystem/door/open";  // The topic to which our client will subscribe
-const char *mqtt_sub_add    = "DoorSystem/card/add";  // The topic to which our client will subscribe
+//const char *mqtt_pub_check  = "DoorSystem/card/check";
+//const char *mqtt_pub_addCard= "DoorSystem/card/add";
+const char *mqtt_sub_open   = "DoorSystem/door/open";
+const char *mqtt_sub_card   = "DoorSystem/card/add";
+const char *mqtt_sub_access = "DoorSystem/access/add";
 
 #endif
